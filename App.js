60dd1447a2b1dc16ae82cdc9b54bdd63006a10c4
lilhovhann <lilith.hovhann@gmail.com>
2021-06-28 18:33:09 +0400

    added navigation button

--- App.js
+++ App.js
@@ -1,23 +1,67 @@
 import React from 'react';
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+import {View, StyleSheet, Button, TextInput, Alert, Text} from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Welcome from './Welcome';
 
-
-
-const App = () => (
-  <SafeAreaView >
-    <View>
-      <Text >
-       Title
-      </Text>
+const Login = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text>Log in</Text>
+      <TextInput style={styles.input} placeholder="user name" />
+      <TextInput style={styles.input} placeholder="password" />
+      <View style={styles.checkboxContainer}>
+        <CheckBox />
+        <Text style={styles.label}>Remember me</Text>
+      </View>
       <Button
-        title="Press me"
-        onPress={() => Alert.alert('Simple Button pressed')}
+        color="#f194ff"
+        title="Log in"
+        onPress={() => navigation.navigate('Welcome')}
       />
     </View>
-    
-  </SafeAreaView>
-);
+  );
+};
 
+const Stack = createStackNavigator();
 
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Welcome" component={Welcome} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+  },
+  button: {
+    color: '#f194ff',
+  },
+  label: {
+    margin: 8,
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+});
 
 export default App;
