f78f0ea30c5d7782ea1c4eea86930eed94011f2b
lilhovhann <lilith.hovhann@gmail.com>
2021-06-30 18:49:37 +0400

    s

--- android/app/src/components/Login.js
+++ android/app/src/components/Login.js
@@ -1,28 +1,43 @@
 import React, {useState} from 'react';
-import {View, Button, TextInput, Alert, Text} from 'react-native';
+import {Image} from 'react-native';
+
+import {
+  View,
+  Button,
+  TextInput,
+  Alert,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
 import styles from '../styles/ComponentStyles';
+import emptyCheck from './Validation';
 
 const Login = ({navigation}) => {
   const [userName, setUserName] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Log in</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="username"
-        onChangeText={username => setUserName(username)}
-      />
-      <TextInput style={styles.input} placeholder="password" />
+      <Image style={styles.logo} source={require('./axele.png')} />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="user name"
+          onChangeText={username => setUserName(username)}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput style={styles.inputText} placeholder="password" />
+      </View>
+
       <View style={styles.checkboxContainer}>
         <CheckBox />
         <Text style={styles.label}>Remember me</Text>
       </View>
-      <Button
-        color="#f194ff"
-        title="Log in"
-        onPress={() => emptyCheck(userName, navigation)}
-      />
+      <TouchableOpacity
+        style={styles.loginBtn}
+        onPress={() => emptyCheck(userName, navigation)}>
+        <Text style={styles.loginText}>Log in</Text>
+      </TouchableOpacity>
     </View>
   );
 };
--- android/app/src/styles/ComponentStyles.js
+++ android/app/src/styles/ComponentStyles.js
@@ -3,27 +3,59 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+  },
+  logo: {
+    marginTop: 100,
+    marginBottom: 50,
+  },
+
+  inputView: {
+    width: '80%',
+    backgroundColor: '#ced6de',
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: 'black',
+  },
+  forgot: {
+    color: 'white',
+    fontSize: 11,
+  },
+  loginBtn: {
+    width: '80%',
+    backgroundColor: '#275AB6',
+    borderRadius: 25,
+    height: 50,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-  },
-  input: {
-    width: 200,
-    height: 44,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'black',
+    marginTop: 10,
     marginBottom: 10,
   },
+  loginText: {
+    color: 'white',
+  },
+
   button: {
     color: '#f194ff',
   },
   label: {
     margin: 8,
+    color: 'black',
   },
   checkboxContainer: {
     flexDirection: 'row',
     marginBottom: 20,
+    color: 'white',
+  },
+  loginText: {
+    color: 'white',
   },
 });
 
